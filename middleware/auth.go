6875828f856bd1b2d2e2137fdc3db9d96f960c2c
Soulaymane Abiadou <abiadou.soulaymane@gmail.com>
2023-09-23 11:12:03 +0100

    add auth middleware for validation tokens and access control

---
+++ middleware/auth.go
@@ -0,0 +1,73 @@
+package middleware
+
+import (
+	"log"
+	"strings"
+
+	"podcast/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token string
+
+		authHeader := c.Request.Header.Get("Authorization")
+		cookieToken, cookieErr := c.Cookie("access_token")
+
+		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer") {
+			token = strings.TrimPrefix(authHeader, "Bearer ")
+		} else if cookieErr == nil {
+			token = cookieToken
+		}
+
+		if token == "" {
+			utils.UnauthorizedResponse(c)
+			return
+		}
+
+		payload, err := utils.ParseAccessJWT(token)
+		if err != nil {
+			log.Println("user access_token unuthorized")
+			utils.UnauthorizedResponse(c)
+			return
+		}
+
+		c.Set("user", payload)
+
+		c.Next()
+	}
+}
+
+func AuthenticateRefreshToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token string
+
+		authHeader := c.Request.Header.Get("X-REFRESH-TOKEN")
+		cookieToken, cookieErr := c.Cookie("refresh_token")
+
+		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer") {
+			token = strings.TrimPrefix(authHeader, "Bearer ")
+		} else if cookieErr == nil {
+			token = cookieToken
+		}
+
+		if token == "" {
+			utils.UnauthorizedResponse(c)
+			return
+		}
+
+		payload, err := utils.ParseRefreshJWT(token)
+
+		if err != nil {
+			log.Println("user refresh_token unuthorized")
+			utils.UnauthorizedResponse(c)
+			return
+		}
+
+		c.Set("user", payload)
+
+		c.Next()
+	}
+}
